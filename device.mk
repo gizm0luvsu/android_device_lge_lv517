427ea2e77463c82ac2cf8c836ac9db9d99fd8dff
dfuse06 <dfuse06@gmail.com>
2017-10-04 22:20:03 -0500

    phone-xhdpi-2048-dalvik-heap

--- device.mk
+++ device.mk
@@ -19,6 +19,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/lge/lv517/lv517-vendor.mk)
 
+$(call inherit-product-if-exists, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
